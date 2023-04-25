e90c9270f8098e2d0ecc32024734897bccbc508b
francogb98 <francobaudino98@gmail.com>
2023-04-25 13:23:47 -0300

    backend

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -3,7 +3,7 @@ const { db } = require("./src/db.js");
 const { PORT } = process.env;
 
 // Syncing all the models at once.
-db.sync({ force: true }).then(() => {
+db.sync({ alter: true }).then(() => {
   server.listen(PORT, () => {
     console.log("%s listening at", PORT); // eslint-disable-line no-console
   });
